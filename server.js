536fab0e2647f03eaf5051ee9d90313ac94af801
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2017-09-04 21:59:25 +0300

    [Feat] Find all books or findByName

--- server.js
+++ server.js
@@ -6,31 +6,41 @@ const Books = require('./db/models/books');
 const app = express();
 const database = 'mongodb://localhost/tweet-all-about-it';
 
-app.set('port', process.env.PORT || 3001); // Whatever PORT is or 3000
+app.set('port', process.env.PORT || 3001); // Whatever PORT is or 3001
 
 app.get('/api/books', (req, res) => {
-  const param = req.query.name;
-  if (!param) {
-    res.json({
-      error: 'Missing parameter `name` in query.',
-    });
-  } else {
-    mongoose.connect(database, {useMongoClient: true}, err => {
-      if (err) {
-        console.log(`Error establishing mongoose connection with db ${err}`);  // eslint-disable-line
+  mongoose.connect(database, { useMongoClient: true }, err => {
+    if (err) {
+      console.log(`Error establishing mongoose connection with db ${err}`); // eslint-disable-line
+    } else {
+      const param = req.query.name;
+      if (!param) {
+        Books.find((err, books) => {
+          if (err) {
+            res.json({
+              error: err,
+              status: 404,
+              message: 'Error finding books from model.'
+            });
+          } else {
+            res.json({ status: 200, data: books });
+          }
+        });
       } else {
-        // mongoose.connection.on('connected', () => {
-          Books.find((err, books) => {
-            if (err) {
-              console.log(`Error finding books from model ${err}`); // eslint-disable-line
-            } else {
-              res.json({ status: 200, books });
-            }
-          });
-        // });
+        Books.find({ name: param }, (err, book) => {
+          if (err) {
+            res.json({
+              error: err,
+              status: 404,
+              message: `Book of name ${param} not found.`
+            });
+          } else {
+            res.json({ status: 200, data: book });
+          }
+        });
       }
-    });
-  }
+    }
+  });
 });
 
 app.listen(app.get('port'), () => {
